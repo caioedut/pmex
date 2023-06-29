cb0c96e26a4a79df1db67457c2779313a3e8e2aa
Caio Teixeira <caioedut@gmail.com>
2023-06-29 16:50:28 -0300

    Big Bang

--- lib/test.ts
+++
@@ -1,1 +0,0 @@
-console.log('Fake server running...');
---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import { execSync, ExecSyncOptions } from 'child_process';
+import { npmCommands, yarnCommands } from './commands';
+
+export type NcxCommand = string | { npm: string; yarn: string };
+
+export type NcxOptions = ExecSyncOptions;
+
+export default function ncx(command: NcxCommand, options?: NcxOptions) {
+  const isYarn = `${process?.env?.npm_execpath || ''}`.toLowerCase().includes('yarn');
+
+  // Set package manager
+  const pm = isYarn ? 'yarn' : 'npm';
+  const args = (typeof command === 'string' ? command : command?.[pm]).trim().split(' ');
+  const isNpx = args?.[0] === 'npx';
+
+  const pmRunner = isNpx ? 'npx' : pm;
+  const pmCommand = args
+    .join(' ')
+    .replace(/^(yarn|npm|npx)\s+/, '')
+    .replace(/^(run)\s+/, '')
+    .trim();
+
+  const pmNative = isNpx || (isYarn ? yarnCommands : npmCommands).includes(pmCommand);
+  const runScript = `${pmRunner}${pmNative ? '' : ' run'} ${pmCommand}`;
+
+  process.stdout.write(`\n`);
+  process.stdout.write(`\x1b[44m`);
+  process.stdout.write(` ${pmRunner} `);
+  if (!pmNative) {
+    process.stdout.write(`\x1b[43m`);
+    process.stdout.write(` run `);
+  }
+  process.stdout.write(`\x1b[100m`);
+  process.stdout.write(` ${pmCommand} `);
+  process.stdout.write(`\x1b[0m`);
+  process.stdout.write(`\n\n`);
+
+  return execSync(runScript, {
+    stdio: 'inherit',
+    encoding: 'utf-8',
+    ...options,
+  });
+}
