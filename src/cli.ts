0b255f0adbe8b5de766c082a4820d730a7bcbe01
Caio Teixeira <caioedut@gmail.com>
2023-06-30 13:05:45 -0300

    fix: removing detection by lockfile because it's kinda buggy

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,7 @@
 #!/usr/bin/env node
 
-import { join } from 'path';
-import { existsSync } from 'fs';
-
 import pmex from './index';
 
 const [, , ...args] = process.argv;
 
-const runners = ['npx', 'npm', 'yarn', 'pnpm'];
-
-let command = args.join(' ').trim();
-
-if (!runners.some((runner) => command.startsWith(`${runner} `))) {
-  // Auto detect package manager
-  const hasNpmLock = existsSync(join(`${process.cwd()}`, 'package-lock.json'));
-  const hasYarnLock = existsSync(join(`${process.cwd()}`, 'yarn.lock'));
-  const hasPnpmLock = existsSync(join(`${process.cwd()}`, 'pnpm-lock.yaml'));
-
-  if ([hasNpmLock, hasYarnLock, hasPnpmLock].filter(Boolean).length > 1) {
-    throw new Error(`Unable to set package manager, you have more than one lockfile.`);
-  }
-
-  command = `${hasPnpmLock ? 'pnpm' : hasYarnLock ? 'yarn' : 'npm'} ${command}`;
-}
-
-pmex(command);
+pmex(args.join(' '));
