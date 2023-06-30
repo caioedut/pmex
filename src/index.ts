cac5a5c404408c133bc9ba931ceb6f561f7adfd2
Caio Teixeira <caio.teixeira@unyleya.com.br>
2023-06-30 12:40:08 -0300

    cli: new detection mode, by lockfile

--- src/index.ts
+++ src/index.ts
@@ -3,40 +3,59 @@ import { join } from 'path';
 import { existsSync, readdirSync } from 'fs';
 
 export default function pmex(command: string | { npm: string; yarn: string; pnpm: string }, options?: ExecSyncOptions) {
-  // Set package manager
   const execPath = `${process?.env?.npm_execpath || ''}`.toLowerCase();
-  const pm = execPath.includes('yarn') ? 'yarn' : execPath.includes('pnpm') ? 'pnpm' : 'npm';
 
-  const binPath = join(`${process.cwd()}`, 'node_modules', '.bin');
-  const binScripts = existsSync(binPath) ? readdirSync(binPath).filter((file) => !file.includes('.')) : [];
+  const runners = ['npx', 'npm', 'yarn', 'pnpm'] as const;
 
-  const pkgPath = join(`${process.cwd()}`, 'package.json');
-  const pkgScripts: string[] = existsSync(pkgPath) ? Object.keys(require(pkgPath)?.scripts ?? {}) : [];
+  let runner: (typeof runners)[number] = execPath.includes('pnpm')
+    ? 'pnpm'
+    : execPath.includes('yarn')
+    ? 'yarn'
+    : 'npm';
 
-  const args = (typeof command === 'string' ? command : command?.[pm]).trim().split(' ');
-  const isNpx = args?.[0] === 'npx';
+  if (typeof command !== 'string') {
+    command = command?.[runner];
+  }
 
-  const pmRunner = isNpx ? 'npx' : pm;
-  const pmCommand = args
-    .join(' ')
-    .replace(/^(npm|yarn|pnpm|npx)\s+/, '')
+  command = `${command ?? ''}`.trim();
+
+  // Check if command replaces package manager detection
+  if (runners.some((runner) => (command as string).startsWith(`${runner} `))) {
+    // @ts-expect-error
+    runner = command.split(' ', 1).shift();
+  }
+
+  command = command
+    .replace(/^(npx|npm|yarn|pnpm)\s+/, '')
     .replace(/^(run)\s+/, '')
     .trim();
 
-  const cmdArg = pmCommand.split(' ').shift() ?? '';
+  // Detect binaries
+  const binPath = join(`${process.cwd()}`, 'node_modules', '.bin');
+  const binScripts = existsSync(binPath) ? readdirSync(binPath).filter((file) => !file.includes('.')) : [];
+
+  // Detect scripts
+  const pkgPath = join(`${process.cwd()}`, 'package.json');
+  const pkgScripts: string[] = existsSync(pkgPath) ? Object.keys(require(pkgPath)?.scripts ?? {}) : [];
+
+  const cmdArg = command.split(' ').shift() ?? '';
   const isBinScript = binScripts.includes(cmdArg);
   const isPkgScript = pkgScripts.includes(cmdArg);
-  const runScript = `${isBinScript ? '' : `${pmRunner} `}${isPkgScript ? 'run ' : ''}${pmCommand}`;
+  const runScript = `${isBinScript ? '' : `${runner} `}${isPkgScript ? 'run ' : ''}${command}`;
 
   process.stdout.write(`\n`);
   process.stdout.write(`\x1b[44m`);
-  process.stdout.write(` ${pmRunner} `);
+  process.stdout.write(` ${runner} `);
+  if (isBinScript) {
+    process.stdout.write(`\x1b[42m`);
+    process.stdout.write(` bin `);
+  }
   if (isPkgScript) {
     process.stdout.write(`\x1b[43m`);
     process.stdout.write(` run `);
   }
   process.stdout.write(`\x1b[100m`);
-  process.stdout.write(` ${pmCommand} `);
+  process.stdout.write(` ${command} `);
   process.stdout.write(`\x1b[0m`);
   process.stdout.write(`\n\n`);
 
