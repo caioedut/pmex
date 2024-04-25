d0ebedd6866a5774505d75f5987335f2234c1f04
Caio Teixeira <caioedut@gmail.com>
2024-04-24 21:59:28 -0300

    feat: new util fn "args"

--- scripts/test.ts
+++ scripts/test.ts
@@ -2,4 +2,4 @@ import pmex from '../src';
 
 pmex(`prettier "{scripts,src}/**/*.{js,jsx,ts,tsx}" --check`);
 
-pmex(`tsc --noEmit`);
+pmex(`tsc --noEmit --skipLibCheck`);
---
+++ src/args.ts
@@ -0,0 +1,49 @@
+export type Args = Omit<{ [key: string]: string | number | boolean }, '_args'> & {
+  _args: string[];
+};
+
+export default function args() {
+  const argv = process.argv?.slice(2) ?? [];
+
+  // @ts-expect-error
+  const result: Args = { _args: [] };
+
+  for (let i = 0; i < argv.length; i++) {
+    const arg = argv[i];
+    const next = argv[i + 1];
+
+    if (!arg.startsWith('-')) {
+      result._args.push(arg);
+      continue;
+    }
+
+    if (arg.startsWith('-')) {
+      const split = arg.split('=');
+
+      let attr: string = split.shift()!.replace(/^-{1,2}/g, '');
+      let value: string | number | boolean = split.join('=');
+
+      if (!split.length && typeof next === 'string' && !next.startsWith('-')) {
+        value = next;
+        i++;
+      }
+
+      if (value === '' || value === 'true') {
+        value = true;
+      }
+
+      if (value === 'false') {
+        value = false;
+      }
+
+      // @ts-expect-error
+      if (typeof value === 'string' && !isNaN(value) && !isNaN(parseFloat(value))) {
+        value = parseFloat(value);
+      }
+
+      result[attr] = value;
+    }
+  }
+
+  return result;
+}
