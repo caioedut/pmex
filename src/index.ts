799ac2b38f7c7431fd55dc3ced77329be7b9a02f
Caio Teixeira <caio.teixeira@unyleya.com.br>
2024-05-15 14:17:33 -0300

    chore: upgrade packages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import pmex from './pmex';
+import args from './args';
 
-export { Command } from './pmex';
-export { default as args, Args } from './args';
+export type { Command } from './pmex';
+export type { Args } from './args';
 
+export { args };
 export default pmex;
